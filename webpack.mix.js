697c132fe5e1aaeccad31a9cc0667a62331618ae
Lenin Paulino <leninpaulinon@gmail.com>
2020-01-28 16:17:35 -0400

    Add tailwindcss (#9)

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,11 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+const tailwindcss = require('tailwindcss')
 
 mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind.config.js') ],
+  });
