84dd9cdfb5b234339cfa0eb0f543a30b21b572ce
Lenin Paulino <leninpaulinon@gmail.com>
2020-01-29 02:01:45 -0400

    Install ziggy

--- resources/views/layouts/inertia.blade.php
+++ resources/views/layouts/inertia.blade.php
@@ -1,58 +1,61 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
-  </head>
-  <body class="bg-gray-100 font-sans leading-normal tracking-normal">
-    @inertia
 
-    <script>
-	/* Progress bar */
-	//Source: https://alligator.io/js/progress-bar-javascript-css-variables/
-	var h = document.documentElement,
-		  b = document.body,
-		  st = 'scrollTop',
-		  sh = 'scrollHeight',
-		  progress = document.querySelector('#progress'),
-		  scroll;
-	var scrollpos = window.scrollY;
-	var header = document.getElementById("header");
-	var navcontent = document.getElementById("nav-content");
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+	<link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+	<script src="{{ mix('/js/app.js') }}" defer></script>
+</head>
 
-	document.addEventListener('scroll', function() {
+<body class="bg-gray-100 font-sans leading-normal tracking-normal">
+	@routes
+	@inertia
 
-	/*Refresh scroll % width*/
-	scroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
-	progress.style.setProperty('--scroll', scroll + '%');
+	<script>
+		/* Progress bar */
+		//Source: https://alligator.io/js/progress-bar-javascript-css-variables/
+		var h = document.documentElement,
+			b = document.body,
+			st = 'scrollTop',
+			sh = 'scrollHeight',
+			progress = document.querySelector('#progress'),
+			scroll;
+		var scrollpos = window.scrollY;
+		var header = document.getElementById("header");
+		var navcontent = document.getElementById("nav-content");
 
-	/*Apply classes for slide in bar*/
-	scrollpos = window.scrollY;
+		document.addEventListener('scroll', function() {
 
-    if(scrollpos > 10){
-      header.classList.add("bg-white");
-	  header.classList.add("shadow");
-	  navcontent.classList.remove("bg-gray-100");
-	  navcontent.classList.add("bg-white");
-    }
-    else {
-	  header.classList.remove("bg-white");
-	  header.classList.remove("shadow");
-	  navcontent.classList.remove("bg-white");
-	  navcontent.classList.add("bg-gray-100");
-	  
-    }
+			/*Refresh scroll % width*/
+			scroll = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+			progress.style.setProperty('--scroll', scroll + '%');
 
-	});
+			/*Apply classes for slide in bar*/
+			scrollpos = window.scrollY;
+
+			if (scrollpos > 10) {
+				header.classList.add("bg-white");
+				header.classList.add("shadow");
+				navcontent.classList.remove("bg-gray-100");
+				navcontent.classList.add("bg-white");
+			} else {
+				header.classList.remove("bg-white");
+				header.classList.remove("shadow");
+				navcontent.classList.remove("bg-white");
+				navcontent.classList.add("bg-gray-100");
+
+			}
+
+		});
 
 
-	//Javascript to toggle the menu
-	document.getElementById('nav-toggle').onclick = function(){
-		document.getElementById("nav-content").classList.toggle("hidden");
-	}
-	
-	
-  </body>
+		//Javascript to toggle the menu
+		document.getElementById('nav-toggle').onclick = function() {
+			document.getElementById("nav-content").classList.toggle("hidden");
+		}
+	</script>
+
+</body>
+
 </html>
